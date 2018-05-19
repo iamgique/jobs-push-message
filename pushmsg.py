cdce7fd4cacf7e2fe05dcac13edefb60fba29dea
Gique <sakul.mon@ascendcorp.com>
2018-05-20 03:02:18 +0700

    add cron

--- pushmsg.py
+++ pushmsg.py
@@ -9,21 +9,21 @@ sched = BlockingScheduler()
 #def timed_job():
     #print('This job is run every three minutes.')
 
-@sched.scheduled_job('cron', day_of_week='*', hour=2, minute=57)
+@sched.scheduled_job('cron', day_of_week='*', hour=20, minute=4)
 def scheduled_job():
     print('This job is run every day at 12pm.')
     r = requests.post(url = API_ENDPOINT, data = '')
     pastebin_url = r.text
     print("The pastebin URL is:%s"%pastebin_url)
 
-@sched.scheduled_job('cron', day_of_week='*', hour=12)
+@sched.scheduled_job('cron', day_of_week='*', hour=5) # 12 - 7 hour
 def scheduled_job():
     print('This job is run every day at 12pm.')
     r = requests.post(url = API_ENDPOINT, data = '')
     pastebin_url = r.text
     print("The pastebin URL is:%s"%pastebin_url)
 
-@sched.scheduled_job('cron', day_of_week='*', hour=18)
+@sched.scheduled_job('cron', day_of_week='*', hour=11) # 18 - 7 hour
 def scheduled_job():
     print('This job is run every day at 18pm.')
     r = requests.post(url = API_ENDPOINT, data = '')
---
+++ readme.md
@@ -0,0 +1,30 @@
+# PUSH-MSG
+- Run on python app using BlockingScheduler and requests
+- This Job run everyday at 12:00pm and 18:00pm in thailand.
+
+## Running Locally
+Make sure you have [python](https://www.python.org) and the [pip](https://pip.pypa.io/en/stable/installing/) installed.
+
+```sh
+$ python pushmsg.py
+```
+
+## Deploying to Heroku
+```
+$ heroku create
+$ git push heroku master
+$ heroku open
+```
+
+## Change source code
+```
+$ heroku git:remote -a {heroku app name}
+$ git add .
+$ git commit -m "First commit"
+$ git push heroku master
+```
+
+## Logs
+```
+$ heroku logs --tail
+```
