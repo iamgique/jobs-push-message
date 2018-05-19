1b032f593d7cdc5308ae19b29a27371da12aea27
Gique <sakul.mon@ascendcorp.com>
2018-05-20 02:46:49 +0700

    add cron

--- pushmsg.py
+++ pushmsg.py
@@ -1,13 +1,33 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
+
+API_ENDPOINT = "https://todo-list-by-gique.herokuapp.com:443/pushMsg"
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('This job is run every three minutes.')
+#@sched.scheduled_job('interval', minutes=1)
+#def timed_job():
+    #print('This job is run every three minutes.')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+@sched.scheduled_job('cron', day_of_week='*', hour=2, minute=49)
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    print('This job is run every day at 12pm.')
+    r = requests.post(url = API_ENDPOINT, data = '')
+    pastebin_url = r.text
+    print("The pastebin URL is:%s"%pastebin_url)
+
+@sched.scheduled_job('cron', day_of_week='*', hour=12)
+def scheduled_job():
+    print('This job is run every day at 12pm.')
+    r = requests.post(url = API_ENDPOINT, data = '')
+    pastebin_url = r.text
+    print("The pastebin URL is:%s"%pastebin_url)
+
+@sched.scheduled_job('cron', day_of_week='*', hour=18)
+def scheduled_job():
+    print('This job is run every day at 18pm.')
+    r = requests.post(url = API_ENDPOINT, data = '')
+    pastebin_url = r.text
+    print("The pastebin URL is:%s"%pastebin_url)
 
 sched.start()
