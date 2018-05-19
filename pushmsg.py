3172215d93f7f5742902c8aa2190f6b7e91dbedc
Gique <sakul.mon@ascendcorp.com>
2018-05-20 03:21:03 +0700

    add cron

--- pushmsg.py
+++ pushmsg.py
@@ -5,17 +5,6 @@ API_ENDPOINT = "https://todo-list-by-gique.herokuapp.com:443/pushMsg"
 
 sched = BlockingScheduler()
 
-#@sched.scheduled_job('interval', minutes=1)
-#def timed_job():
-    #print('This job is run every three minutes.')
-
-@sched.scheduled_job('cron', day_of_week='*', hour=20, minute=6)
-def scheduled_job():
-    print('This job is run every day at 12pm.')
-    r = requests.post(url = API_ENDPOINT, data = '')
-    pastebin_url = r.text
-    print("The pastebin URL is:%s"%pastebin_url)
-
 @sched.scheduled_job('cron', day_of_week='*', hour=5) # 12 - 7 hour
 def scheduled_job():
     print('This job is run every day at 12pm.')
